bdefd5ce412914d09be1954f9c449abc4a3cd625
Collin <collinvaccaro00@gmail.com>
2020-06-08 18:23:26 -0600

    express server basic

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+
+
+app.listen(PORT, () => console.log(`localhost:${PORT}`))
